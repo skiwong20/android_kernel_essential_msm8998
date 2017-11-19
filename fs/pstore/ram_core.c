d1d1f986ab97cd9efd6d6796d13c97fa4c1ec133
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2017-11-19 02:22:46 +0100

    Merge tag 'v4.4.80' of https://source.codeaurora.org/quic/la/kernel/msm-4.4 into HEAD
    This is the 4.4.80 stable release

--- fs/pstore/ram_core.c
+++ fs/pstore/ram_core.c
@@ -48,16 +48,15 @@ static inline size_t buffer_start(struct persistent_ram_zone *prz)
 	return atomic_read(&prz->buffer->start);
 }
 
-static DEFINE_RAW_SPINLOCK(buffer_lock);
-
 /* increase and wrap the start pointer, returning the old value */
 static size_t buffer_start_add(struct persistent_ram_zone *prz, size_t a)
 {
 	int old;
 	int new;
-	unsigned long flags;
+	unsigned long flags = 0;
 
-	raw_spin_lock_irqsave(&buffer_lock, flags);
+	if (!(prz->flags & PRZ_FLAG_NO_LOCK))
+		raw_spin_lock_irqsave(&prz->buffer_lock, flags);
 
 	old = atomic_read(&prz->buffer->start);
 	new = old + a;
